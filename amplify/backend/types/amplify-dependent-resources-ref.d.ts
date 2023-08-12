d4bc7667c10afcceddba6bded3378eeaae0ee1aa
EC2 Default User <ec2-user@ip-172-31-6-153.ap-northeast-2.compute.internal>
2023-08-12 07:13:38 +0000

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,40 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyworkshopapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "EcommerceAuth": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "AmplifyWorkshopLambdaItems": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dynamodbitems": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import Amplify from 'aws-amplify'
+//import * as serviceWorker from './serviceWorker';
+
+import AppProvider from './context/AppProvider'
+
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Product from './pages/Product'
+import Checkout from './pages/Checkout'
+import PlacedOrder from './pages/PlacedOrder'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const routing = (
+    <AppProvider>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/product/:id" component={Product} />
+                <Route path="/checkout" component={Checkout} />
+                <Route path="/ordercomplete" component={PlacedOrder}/>
+                <Route component={App} />
+            </Switch>
+        </Router>
+    </AppProvider>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
+
+//serviceWorker.unregister();
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// import Amplify from 'aws-amplify';
+// import awsconfig from './aws-exports';
+
+// Amplify.configure(awsconfig);
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
